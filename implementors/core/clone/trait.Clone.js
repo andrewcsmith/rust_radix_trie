178eeb7e456e1477aacb244b2fedcec4469a308c
Michael Sproul <micsproul@gmail.com>
2015-06-26 05:38:35 +0000

    Rebuild pages at b541a31.

--- implementors/core/clone/trait.Clone.js
+++ implementors/core/clone/trait.Clone.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['radix_trie'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='radix_trie/struct.NibbleVec.html' title='radix_trie::NibbleVec'>NibbleVec</a>",];
+implementors['radix_trie'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='radix_trie/struct.NibbleVec.html' title='radix_trie::NibbleVec'>NibbleVec</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/core/cmp/trait.Eq.js
+++ implementors/core/cmp/trait.Eq.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['radix_trie'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='struct' href='radix_trie/struct.NibbleVec.html' title='radix_trie::NibbleVec'>NibbleVec</a>",];
+implementors['radix_trie'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='struct' href='radix_trie/struct.NibbleVec.html' title='radix_trie::NibbleVec'>NibbleVec</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
