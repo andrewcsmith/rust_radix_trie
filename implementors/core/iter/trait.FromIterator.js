3c656f6174e8d5a01af7a498a4154712e28d248b
Michael Sproul <micsproul@gmail.com>
2015-07-07 12:56:05 +0000

    Rebuild pages at 223a77b.

---
+++ implementors/core/iter/trait.FromIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['radix_trie'] = ["impl&lt;K, V&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.FromIterator.html' title='core::iter::FromIterator'>FromIterator</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.tuple.html'>(K, V)</a>&gt; for <a class='struct' href='radix_trie/struct.Trie.html' title='radix_trie::Trie'>Trie</a>&lt;K, V&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
