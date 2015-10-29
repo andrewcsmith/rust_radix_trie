68ab12805ba1c18738585936e3480980484f7724
Michael Sproul <micsproul@gmail.com>
2015-10-29 10:15:25 +0000

    Rebuild pages at 830ac9f.

--- implementors/core/iter/trait.FromIterator.js
+++ implementors/core/iter/trait.FromIterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['radix_trie'] = ["impl&lt;K, V&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.FromIterator.html' title='core::iter::FromIterator'>FromIterator</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.tuple.html'>(K, V)</a>&gt; for <a class='struct' href='radix_trie/struct.Trie.html' title='radix_trie::Trie'>Trie</a>&lt;K, V&gt;",];
+implementors['radix_trie'] = ["impl&lt;K, V&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.FromIterator.html' title='core::iter::FromIterator'>FromIterator</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.tuple.html'>(K, V)</a>&gt; for <a class='struct' href='radix_trie/struct.Trie.html' title='radix_trie::Trie'>Trie</a>&lt;K, V&gt; <span class='where'>where K: <a class='trait' href='radix_trie/trait.TrieKey.html' title='radix_trie::TrieKey'>TrieKey</a></span>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
